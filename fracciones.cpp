f42ff3f23ecfbc444f5d061e405fb9cbd2d00ac7
IvonneEstrada <ivonne_montserrat@outlook.com>
2018-05-09 04:50:24 +0000

    Ejercicio fracciones terminado

--- fracciones.cpp
+++ fracciones.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 
-class Fraccion
-{
-private:
-    int numerador;
-    int denominador;
-        
-public: 
-    Fraccion(int numerador, int denominador);
-    Fraccion operator+(Fraccion otra); //sobrecarga de metodo miembro
-    Fraccion operator-(Fraccion otra);
-    Fraccion operator*(Fraccion otra);
-    Fraccion operator/(Fraccion otra);
-    void mostrar();
-    friend void operator <<(std::ostream &salida, Fraccion f);
-    friend void operator >>(std::istream &entrada, Fraccion &f);
+class Fraccion  {
 
+    private:
+        int numerador;
+        int denominador;
+        
+    public: 
+        Fraccion( int numerador = 1, int denominador = 1 );
+        Fraccion operator+( Fraccion otra );//sobrecarga de metodo miembro
+        Fraccion operator-( Fraccion otra );
+        Fraccion operator*( Fraccion otra );
+        Fraccion operator/( Fraccion otra );
+        void mostrar();
+    
+    friend void operator<< ( std::ostream &salida, Fraccion f );
+    friend void operator>> ( std::istream &entrada, Fraccion &f );
+    
 }; // punto y coma obligatorio
 
-Fraccion::Fraccion(int a, int b)
+Fraccion::Fraccion( int a, int b )
 {
     if(b==0){
         exit(-1);
     }
     else    {
-        numerador = a;
-        denominador = b;
+        this->numerador = a;
+        this->denominador = b;
     }
 }
 
@@ -37,20 +38,18 @@ Fraccion Fraccion::operator+(Fraccion otra)
     return resultado;
 }
 
-void operator <<(std::ostream &salida, Fraccion f)
+void operator<<( std::ostream &salida, Fraccion f )
 {
     salida<<f.numerador<<"\n--"<<"\n"<<f.denominador<<"\n";
 }
 
-void operator >>(std::istream &entrada, Fraccion &f)
+void operator>>( std::istream &entrada, Fraccion &f )
 {
-    std::cout<<"numerador:";
-    entrada>>f.numerador;
-    std::cout<<"denominador:";
-    entrada>>f.denominador;
+    entrada >> f.numerador ;
+    entrada >> f.denominador ;
 }
 
-Fraccion Fraccion:operator-(Fraccion otra)
+Fraccion Fraccion::operator-(Fraccion otra)
 {
     Fraccion resultado;
     resultado.numerador = ((this->numerador*otra.denominador) - (this->denominador*otra.numerador)); 
@@ -74,32 +73,22 @@ Fraccion Fraccion::operator/(Fraccion otra)
    return resultado;
 }
 
-void Fraccion::mostrar()
+/*void Fraccion::mostrar()
 {
     std::cout<<this->numerador<<std::endl;
     std::cout<<"--"<<std::endl;
     std::cout<<this->denominador<<std::endl;
     std::cout<<std::endl;
            
-}
+}*/
 
 int main()
 {
-    Fraccion f1{1,1};
-    std::cin>>f1;
-    std::cout<<f1;
-    Fraccion f2{1,1};
-    std::cin>>f1;
-    std::cout<<f1;
-    Fraccion f3{1,1};
-    f3=f1+f2;
-    std::cout<<f3;
-    f3=f1-f2;
-    std::cout<<f3;
-    f3=f1*f2;
-    std::cout<<f3;
-    f3=f1/f2;
-    std::cout<<f3;
-    return 0;
+    Fraccion f1, f2, f3 ;
+     
+    std::cin >> f1 ;
+    std::cin >> f2 ;
+    f3 = f1 + f2 ;
+    std::cout << f3 ;
 }
 
